6fb3bd2101cba018317bf082f4e19e0a6f667a7c
andreidasilvamoreira <dedeigalinha@hotmail.com>
2026-01-24 08:34:06 -0300

    feat: adicionado repositorio de Pagamento

--- app/Domains/Financeiro/Repositories/PagamentoRepository.php
+++ app/Domains/Financeiro/Repositories/PagamentoRepository.php
@@ -2,7 +2,34 @@
 
 namespace App\Domains\Financeiro\Repositories;
 
+use App\Models\Pagamento;
+use Illuminate\Support\Collection;
+
 class PagamentoRepository
 {
+    public function findAll(): Collection
+    {
+        return Pagamento::all();
+    }
 
+    public function find($id) : Pagamento
+    {
+        return Pagamento::find($id);
+    }
+
+    public function create(array $data) : Pagamento
+    {
+        return Pagamento::create($data);
+    }
+
+    public function update(Pagamento $pagamento, array $data) : Pagamento
+    {
+        $pagamento->update($data);
+        return  $pagamento;
+    }
+
+    public function delete(Pagamento $pagamento) : void
+    {
+        $pagamento->delete();
+    }
 }
